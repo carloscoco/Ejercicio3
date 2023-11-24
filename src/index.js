ee9dc6d56e8787355466c0394e2feb1ce04ced35
carloscoco <128407690+carloscoco@users.noreply.github.com>
2023-11-24 10:04:54 -0300

    json4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import { Divh, Header } from './Components/Header/Header';
 import { Footer } from './Components/Footer/Footer';
 import { Formulario } from './Components/Form/Formulario';
